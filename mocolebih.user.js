3ca2269de042ac748b0825e8a17cad3dd055838d
vachzar <vachzar@gmail.com>
2015-12-31 23:23:24 +0700

    add viva.co.id

--- mocolebih.user.js
+++ mocolebih.user.js
@@ -7,18 +7,20 @@
 // @icon           https://raw.githubusercontent.com/vachzar/mocolebih/master/img/logo.png
 // @updateURL      https://raw.githubusercontent.com/vachzar/mocolebih/master/mocolebih.meta.js
 // @downloadURL    https://raw.githubusercontent.com/vachzar/mocolebih/master/mocolebih.user.js
-// @version        1.0
+// @version        1.1
 // @license        DBAD
 // @grant          GM_addStyle
 // @author         vachzar
 // @require        http://code.jquery.com/jquery-1.10.1.min.js
 // @run-at         document-start
 // @include        http://*.tribunnews.com/*
+// @include        http://*.viva.co.id/news/read/*
 // allow pasting
 // ==/UserScript==
 
 $( document ).ready(function() {
   Baca(/tribunnews.com/, Tribun);
+  Baca(/viva.co.id/, Viva);
 });
 
 function Baca(check, callback) {
@@ -30,7 +32,6 @@ function Baca(check, callback) {
 
 var Tribun = function(){
   var links = [];
-  var currentPage = 1;
   $(document).find('.paging a').each(function () {
     links.push($(this).attr('href'));
   });
@@ -46,4 +47,23 @@ var Tribun = function(){
     $('.paging').prev('div').hide();
     $('.paging').hide();  
     $('.txt-article').append( "<p><b>"+links.length+" Pages Loaded by MocoLebih </b></p>" );
+};
+
+var Viva = function(){
+  var links = [];
+  $(document).find('.pagelist a.page').each(function () {
+    links.push($(this).attr('href'));
+  });
+  
+  $.each(links, function( index, value ) {
+    if(index != 0){
+      var data = $.ajax({url: value, data: {param1: value, param2: value}, async: false}).responseText; 
+      $(data).find('#article-content > span').each(function(){
+        $('#article-content > span').append($(this).html());
+      });
+    }
+  });  
+    $('.pagelist').prev('div').hide();
+    $('.pagelist').hide();  
+    $('#article-content').append( "<p><b>"+links.length+" Pages Loaded by MocoLebih </b></p>" );
 };
